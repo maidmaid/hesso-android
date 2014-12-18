e8b2fb032c93c91f5520789484dfa49295f8a18b
Dany <danymaillard93b@gmail.com>
2014-12-18 20:38:50 +0100

    organize import

--- app/src/main/java/com/maid/csp/Activity/PlanningEditorActivity.java
+++ app/src/main/java/com/maid/csp/Activity/PlanningEditorActivity.java
@@ -1,14 +1,7 @@
 package com.maid.csp.Activity;
 
-import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 import android.widget.DatePicker;
 
 import com.maid.csp.Db.Db;
